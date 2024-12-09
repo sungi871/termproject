f5a689969bc083fc93ac8b498c97ec238cc69f98
tjtldus <siyeon513@gmail.com>
2024-12-09 14:56:36 +0900

    Update color_detection.py

--- color_detection202335235/color_detection.py
+++ color_detection202335235/color_detection.py
@@ -3,7 +3,7 @@ import cv2
 def detect_color(frame, color_range):
    
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # BGR -> HSV 변환
-    
+    #색상 감지
  
     mask = cv2.inRange(hsv_frame, color_range['lower'], color_range['upper'])
     
